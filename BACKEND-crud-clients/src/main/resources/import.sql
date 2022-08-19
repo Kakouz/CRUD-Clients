ad5e6620806543c6fdf4d4a5f789a9b59252f927
Tiago <tf.prudente@gmail.com>
2022-08-19 15:59:58 -0300

    SQL Values

---
+++ BACKEND-crud-clients/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO clients(id,cpf,email,name) VALUES (1, "14910650792", "tiago.prudente@teste.com", "Tiago Felix Prudente");
+INSERT INTO clients(id,cpf,email,name) VALUES (2, "56345680052", "jefferson.rebouças@teste.com", "Jefferson Patrício Rebouças");
+INSERT INTO clients(id,cpf,email,name) VALUES (3, "19127733092", "cristovao.botelho@teste.com", "Cristovão Seixas Botelho");
+INSERT INTO clients(id,cpf,email,name) VALUES (4, "53661485024", "Rahyssa.rios@teste.com", "Rahyssa Figueira Rios");
+INSERT INTO clients(id,cpf,email,name) VALUES (5, "31151638005", "yaroslav.borja@teste.com", "Yaroslav Beltrão Borja");
+INSERT INTO clients(id,cpf,email,name) VALUES (6, "94838487096", "anaya.roriz@teste.com", "Anaya Flores Roriz");
+INSERT INTO clients(id,cpf,email,name) VALUES (7, "68063398084", "poliana.parente@teste.com", "Poliana Cavaco Parente");
+INSERT INTO clients(id,cpf,email,name) VALUES (8, "32962636080", "tyles.lourenço@teste.com", "Tyler Monforte Lourenço");
+INSERT INTO clients(id,cpf,email,name) VALUES (9, "37948025006", "luisa.lancastre@teste.com", "Luisa Freitas Lancastre");
+INSERT INTO clients(id,cpf,email,name) VALUES (10, "01359085076", "melissa.robalinho@teste.com", "Melissa Lemes Robalinho");
+INSERT INTO clients(id,cpf,email,name) VALUES (11, "16927372016", "kristian.taborda@teste.com", "Kristian Matosinhos Taborda");
+INSERT INTO clients(id,cpf,email,name) VALUES (12, "59390192005", "ezio.quintal@teste.com", "Ezio Frias Quintal");
+INSERT INTO clients(id,cpf,email,name) VALUES (13, "83474180045", "simona.valerio@teste.com", "Simona Vigário Valério");
+INSERT INTO clients(id,cpf,email,name) VALUES (14, "21060005026", "nicola.pastana@teste.com", "Nicola Figueira Pastana");
+INSERT INTO clients(id,cpf,email,name) VALUES (15, "25509408065", "vilma.ventura@teste.com", "Vilma Marçal Ventura");
